58a9e5278822e7000f843e00f48b2bed6c5ea617
blvckcoffee <joe.johnson.dev@gmail.com>
2021-01-26 12:55:54 -0600

    Completed practice exercises on basics of JavaScript Operators

--- app.js
+++ app.js
@@ -1,19 +1,50 @@
-console.log("Hello World!\n==========\n");
+console.log('Hello World!\n==========\n')
 console.log(
-  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
-);
+  'Follow the steps in the README.md file to complete the exercises:\n==========\n'
+)
 
 // Exercise 1
-console.log("EXERCISE 1:\n==========\n");
+console.log('EXERCISE 1:\n==========\n')
 
-// YOUR CODE HERE
+//let a = 20
+//let b = 4
+//let add = a + b
+//let minus = a - b
+//let multiply = a * b
+//let dividing = a / b
 
 // Exercise 2
-console.log("EXERCISE 2:\n==========\n");
+console.log('EXERCISE 2:\n==========\n')
 
-// YOUR CODE HERE
+//let a = 11
+//let str = '11'
+//let str2 = 'eleven'
+//let isPresent = true
+//let firstName = 'Jackie'
+//let lastName = 'Chan'
+
+//What is the value of: a + str? 1111
+//What is the value of: a + str2? 11eleven
+//What is the value of: a + isPresent? 12
+//What is the value of: a + firstName? 11Jackie
+//What is the value of: a + lastName? 11Chan
 
 // Exercise 3
-console.log("EXERCISE 3:\n==========\n");
+console.log('EXERCISE 3:\n==========\n')
 
-// YOUR CODE HERE
+//let a = 5
+//let str = '5'
+//let str2 = 'five'
+//let isPresent = false
+//let firstName = 'Robin'
+//let lastName = 'Williams'
+
+//What is the value of: a == str? true
+//What is the value of: a === str? false
+//What is the value of: !isPresent? true
+//What is the value of: (“eleven” == str2 && a >= str)? false
+//What is the value of: (!isPresent || isPresent)? true
+//What is the value of: 0 == false? true
+//What is the value of: 0 === false? false
+//What is the value of: 0 != false? false
+//What is the value of 0 !== false? true
